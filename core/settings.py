f361e123a00a2f0f1efba2db97e1b920a1c5488c
jparaya-neuronia <jparaya@neuronia.cl>
2024-08-03 22:55:42 -0400

    settings

--- core/settings.py
+++ core/settings.py
@@ -102,15 +102,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 #############################################################
 # SRC: https://devcenter.heroku.com/articles/django-assets
